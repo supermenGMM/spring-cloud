e496e43a48f84911d4726b1642f15d3d89d2f114
supermenG <852458550@qq.com>
2018-10-18 17:11:07 +0800

    eureka学习。htt模式的微服务

---
+++ order-service/src/main/java/com/mm/OrderServiceApplication.java
@@ -0,0 +1,18 @@
+package com.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+}
---
+++ product-service/src/main/java/com/mm/pojo/Product.java
@@ -0,0 +1,59 @@
+package com.mm.pojo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue
+    private  int id;
+    private int num;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", num=" + num +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Product(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+}
