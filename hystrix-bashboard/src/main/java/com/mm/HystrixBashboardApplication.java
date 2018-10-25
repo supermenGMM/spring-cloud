b831816cc6a69282c91322449913367895213979
supermenG <852458550@qq.com>
2018-10-25 19:37:38 +0800

    hystrix  监控数据集合  通过http收集聚合。 注意: 配置cluster时，字符串default ，如果它在YAML中，则使用单引号并使用双引号进行转义

--- hystrix-bashboard/src/main/java/com/mm/HystrixBashboardApplication.java
+++ hystrix-bashboard/src/main/java/com/mm/HystrixBashboardApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
-
 @EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixBashboardApplication {
