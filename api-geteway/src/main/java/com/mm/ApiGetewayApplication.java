656ff9bc86d1000e82dac3bbeb6515b05b568d77
supermenG <852458550@qq.com>
2018-10-26 18:10:40 +0800

    网关。路由配置，过滤器

--- api-geteway/src/main/java/com/mm/ApiGetewayApplication.java
+++ api-geteway/src/main/java/com/mm/ApiGetewayApplication.java
@@ -1,12 +1,19 @@
 package com.mm;
 
+import com.mm.Filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGetewayApplication {
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGetewayApplication.class, args);
